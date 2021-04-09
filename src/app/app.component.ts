4980088e0294b20d697b5bec4ae0035798fafb7e
10161998 <82113149+10161998@users.noreply.github.com>
2021-04-09 09:39:07 -0500

    subida del proyecto
    subi el proyecto sin la carpeta de nodemodules

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { areAllEquivalent } from '@angular/compiler/src/output/output_ast';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'proangular';
+  public saludar(){
+ alert("Hola");
+  }
+  
+}
